e8aa1c43297a382a09ddee5d52152a28e5d12ce7
dgjapi <3678869403@qq.com>
2026-07-08 14:49:27 +0800

    Add files via upload

--- api/tkjs/tkinterjs.js
+++ api/tkjs/tkinterjs.js
@@ -1,5 +1,10 @@
+
+
 class tk {
 	static Tk = function(){return document.body;}
+	static title(tit){
+		document.title = tit;
+	}
 	
 	static Frame(master,class_="tk.frame",id="tk",width=100,height=100){
 		let a = document.createElement("div");
@@ -8,7 +13,7 @@ class tk {
 		a.className = class_;
 		a.id = id;
 		master.appendChild(a);
-		return a
+		return a;
 	}
 	
 	static Label(master,class_="tk.label",id="tk",text="hello tkjs!"){
@@ -24,6 +29,56 @@ class tk {
 		let a = document.createElement("button");
 		a.textContent = text;
 		a.onclick = onclick;
+		a.className = class_;
+		a.id = id;
+		master.appendChild(a);
+		return a;
+	}
+
+	static Entry(master,class_="tk.entry",id="tk"){
+		let a = document.createElement("input");
+		a.type = "text";
+		a.className = class_;
+		a.id = id;
+		master.appendChild(a);
+		return a;
+	}
+
+	static Text(master,class_="tk.text",id="tk",width=50,height=50){
+		let a = document.createElement("textarea");
+		a.style.width = width + "ch";
+		a.style.height = height + "em";
+		a.className = class_;
+		a.id = id;
+		master.appendChild(a);
+		return a;
+	}
+
+	static Listbox = class {
+		constructor(master,class_,id) {
+			let a = document.createElement("ul");
+			a.className = class_;
+			a.id = id;
+			master.appendChild(a);
+			this.a = a;
+		}
+		appEnd(ele){
+			this.a.appendChild(ele);
+		}
+		deleteEnd(){
+			let lastEle = this.a.lastElementChild;
+			if (lastEle)
+			{
+				this.a.removeChild(lastEle);
+			}
+		}
+		getObj(){
+			return this.a;
+		}
+	}
+	static ListboxLine(master,class_,id,ele){
+		let a = document.createElement("li");
+		a.textContent = text;
 		a.className = class_;
 		a.id = id;
 		master.appendChild(a);
